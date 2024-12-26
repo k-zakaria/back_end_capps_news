43d2c0b297d9e4a88757706bd9f001fa1071b50f
k-zakaria <kacimizakaria28@gmail.com>
2024-12-26 10:21:10 +0100

    Ajouter des modification

---
+++ src/main/java/com/capps/BackEndCappsNewsApplication.java
@@ -0,0 +1,13 @@
+package com.capps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCappsNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndCappsNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capps/BackEndCappsNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCappsNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
