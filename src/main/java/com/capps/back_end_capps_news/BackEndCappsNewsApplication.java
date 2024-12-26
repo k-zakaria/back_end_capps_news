9e9768592736f68ce124aca9ac3f2ac3038d875a
k-zakaria <kacimizakaria28@gmail.com>
2024-12-26 10:21:10 +0100

    Install enverenement the project Capps News

---
+++ src/main/java/com/capps/back_end_capps_news/BackEndCappsNewsApplication.java
@@ -0,0 +1,13 @@
+package com.capps.back_end_capps_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCappsNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndCappsNewsApplication.class, args);
+    }
+
+}
