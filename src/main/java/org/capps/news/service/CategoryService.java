6a21b12d193a3148c4de6c3800aab4ee70c2e946
k-zakaria <kacimizakaria28@gmail.com>
2025-03-26 15:18:53 +0000

    add the methode to category

--- src/main/java/org/capps/news/service/CategoryService.java
+++ src/main/java/org/capps/news/service/CategoryService.java
@@ -3,13 +3,14 @@ package org.capps.news.service;
 import org.capps.news.model.Category;
 import org.capps.news.repository.CategoryRepository;
 import org.capps.news.web.exception.category.CategoryNotFoundException;
+import org.capps.news.service.interfaces.CategoryServiceInterface;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CategoryService {
+public class CategoryService implements CategoryServiceInterface {
 
     private final CategoryRepository categoryRepository;
 
@@ -17,18 +18,22 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Override
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
+    @Override
     public Optional<Category> getCategoryById(Long id) {
         return categoryRepository.findById(id);
     }
 
+    @Override
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
     }
 
+    @Override
     public Category updateCategory(Long id, Category categoryDetails) {
         Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
@@ -37,6 +42,7 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
+    @Override
     public void deleteCategory(Long id) {
         categoryRepository.deleteById(id);
     }
