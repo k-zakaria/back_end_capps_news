91f88f46f4eda801c770e36702f296df899326c9
k-zakaria <kacimizakaria28@gmail.com>
2025-03-26 15:23:23 +0000

    add the methode to Unauthorization

---
+++ src/main/java/org/capps/news/web/exception/artilce/UnauthorizedPublicationException.java
@@ -0,0 +1,7 @@
+package org.capps.news.web.exception.artilce;
+
+public class UnauthorizedPublicationException extends RuntimeException {
+    public UnauthorizedPublicationException(String s) {
+        super(s);
+    }
+}
